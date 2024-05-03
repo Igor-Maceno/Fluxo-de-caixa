68b51c54630434352feaafd28b101f9be5714194
Igor-Maceno <igorlopesmaceno17@gmail.com>
2024-05-03 00:10:07 -0300

    navbar

--- cash-flow/src/components/navbar/Navbar.js
+++ cash-flow/src/components/navbar/Navbar.js
@@ -2,7 +2,16 @@ import React from "react";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={styles.container}>
+      <ul>
+        <li>Fluxo de caixa</li>
+        <li>Relatório</li>
+        <li>Configurações</li>
+        <li>Sair</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
